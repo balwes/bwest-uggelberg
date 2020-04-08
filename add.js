36a37d50467387a01ec81886e987005b99f7e248
unknown <balthazar.west@hotmail.com>
2020-04-08 15:48:46 +0200

    Change url_to_json to pass all tests

--- add.js
+++ add.js
@@ -5,9 +5,14 @@ function add(a, b) {
 }
 
 async function url_to_json(url) {
-    const response = await fetch(url);
-    const data = await response.json();
-    return data;
+	try {
+    	const response = await fetch(url);
+    	const data = await response.json();
+		return data;
+	} catch (error) {
+		// Bad URL or malformed JSON
+		return null;
+	}
 }
 
 module.exports = {
