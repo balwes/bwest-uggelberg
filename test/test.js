250c109d8e7e49a8c904a6c7e4a4abbb3810d548
unknown <balthazar.west@hotmail.com>
2020-04-08 15:29:04 +0200

    Change tests

--- test/test.js
+++ test/test.js
@@ -10,29 +10,29 @@ describe("add", function () {
 });
 
 describe("url_to_json", function () {
-    it("return null in the case of a bad link", function () {
-        var json = main.url_to_json("bad link");
-        assert(json === null);
+    it("return null in the case of a bad link", async function () {
+        var json = await main.url_to_json("bad link");
+        expect(json).to.be.equal(null);
     });
-    it("return null if null is given", function () {
-        var json = main.url_to_json(null);
-        assert(json === null);
+    it("return null if null is given", async function () {
+        var json = await main.url_to_json(null);
+        expect(json).to.be.equal(null);
     });
-    it("return null for an empty string", function () {
-        var json = main.url_to_json("");
-        assert(json === null);
+    it("return null for an empty string", async function () {
+        var json = await main.url_to_json("");
+        expect(json).to.be.equal(null);
     });
-    it("return null given a valid link to non-json data", function () {
+    it("return null given a valid link to non-json data", async function () {
         var link = "https://duckduckgo.com/";
-        var json = main.url_to_json("link");
-        assert(json === null);
+        var json = await main.url_to_json("link");
+        expect(json).to.be.equal(null);
     });
-    it("return json data given a valid link to json data", function () {
+    it("return json data given a valid link to json data", async function () {
         var link = "https://pomber.github.io/covid19/timeseries.json";
-        var json = main.url_to_json(link);
+        var json = await main.url_to_json(link);
         // Accessing a non-existent member should return undefined
-        assert(json.foobar === undefined);
+        expect(json.foobar).to.be.equal(undefined);
         // Accessing an existent member should not return undefined
-        assert(json.Sweden !== undefined);
+        expect(json.Sweden).to.not.be.equal(undefined);
     });
 });
