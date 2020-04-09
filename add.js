1b51fa908282eacb23d883249d7bc7ce6a6d103f
unknown <balthazar.west@hotmail.com>
2020-04-09 14:35:15 +0200

    Add implementation

--- add.js
+++ add.js
@@ -15,8 +15,28 @@ async function url_to_json(url) {
 	}
 }
 
-function get_sir_from_date(population, dataset, date) {
-	// foobar!
+/*
+ * Below is the format of date. HOWEVER! The month and day is not
+ * padded to be two digits.
+ * Input: "YYYY-MM-DD"
+ * Output: [S,I,R], or [-1,-1,-1] if the input was bad.
+ */
+function get_sir_from_date(population, data_set, date) {
+	console.log(data_set.length);
+    for (var i = 0; i < data_set.length; i++) {
+		let day_data = data_set[i];
+        if (day_data.date == date) {
+            let sir = [];
+            // susceptible
+            sir[0] = population - day_data.confirmed - day_data.recovered;
+            // infectious
+            sir[1] = day_data.confirmed;
+            // removed
+            sir[2] = day_data.recovered;
+            return sir;
+        }
+    }
+    return [-1,-1,-1];
 }
 
 module.exports = {
