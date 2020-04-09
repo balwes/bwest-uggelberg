3954fd6bc73a95e12b60a0d7d74efa27e457f921
unknown <balthazar.west@hotmail.com>
2020-04-09 14:34:58 +0200

    Fix tests

--- test/test.js
+++ test/test.js
@@ -37,24 +37,22 @@ describe("url_to_json", function () {
     });
 });
 
-describe("get_sir_from_date", function() {
-    const pop = 1000
-    const dataset = {
-        "Sweden": [
-            {
-                "date": "2020-1-22",
-                "confirmed": 10,
-                "deaths": 20,
-                "recovered": 30,
-            },
-            {
-                "date": "2020-1-23",
-                "confirmed": 100,
-                "deaths": 200,
-                "recovered": 300,
-            }
-        ]
-    };
+describe("get_sir_from_date", async function() {
+    const pop = 1000;
+    const dataset = [
+        {
+            "date": "2020-1-22",
+            "confirmed": 10,
+            "deaths": 20,
+            "recovered": 30,
+        },
+        {
+            "date": "2020-1-23",
+            "confirmed": 100,
+            "deaths": 200,
+            "recovered": 300,
+        }
+    ];
     it("return [-1,-1,-1] from a non-existent date", async function() {
         var date = "2100-1-1";
         var sir = main.get_sir_from_date(pop, dataset, date);
@@ -68,13 +66,13 @@ describe("get_sir_from_date", function() {
     it("return SIR data from a correct date", async function() {
         var date = "2020-1-22";
         var sir = main.get_sir_from_date(pop, dataset, date);
-        expect(sir[0]).to.be.greaterThan(pop-10);
-        expect(sir[1]).to.be.greaterThan(10);
-        expect(sir[2]).to.be.greaterThan(30);
+        expect(sir[0]).to.be.equal(pop-10-30);
+        expect(sir[1]).to.be.equal(10);
+        expect(sir[2]).to.be.equal(30);
         
         var date = "2020-1-23";
         var sir = main.get_sir_from_date(pop, dataset, date);
-        expect(sir[0]).to.be.equal(pop-100);
+        expect(sir[0]).to.be.equal(pop-100-300);
         expect(sir[1]).to.be.equal(100);
         expect(sir[2]).to.be.equal(300);
     });
