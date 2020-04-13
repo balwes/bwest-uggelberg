ae78edc6aeb27fe9f11b41ddcc8fd73dab37b552
unknown <balthazar.west@hotmail.com>
2020-04-13 16:05:31 +0200

    Change tests

--- script.js
+++ script.js
@@ -29,24 +29,12 @@ function get_index_of_date(dataset, date) {
 }
 
 /*
- * Return [S,I,R] from a given date. The date format is YYYY-MM-DD,
- * HOWEVER do not pad single-digit months and days with zeroes.
- * [-1,-1,-1] is returned if the input is bad.
+ * Return [S,I,R] from a given index.
+ * Output: [S,I,R], or [-1,-1,-1] if the input was bad.
+ * 
  */
-function get_sir_from_date(population, dataset, date) {
-    var i = get_index_of_date(dataset, date)
-    if (i == -1) {
-        return [-1,-1,-1];
-    }
-    let data = dataset[i];
-    let sir = [];
-    // susceptible
-    sir[0] = population - data.confirmed - data.recovered;
-    // infectious
-    sir[1] = data.confirmed;
-    // removed
-    sir[2] = data.recovered;
-    return sir;
+function get_sir_from_index(population, dataset, index) {
+    //
 }
 
 /*
@@ -62,7 +50,7 @@ function get_population(data) {
 
 module.exports = {
     url_to_covid_data, url_to_population_data, url_to_json,
-    get_sir_from_date, get_population, get_index_of_date
+    get_sir_from_index, get_population, get_index_of_date
 }
 
 
