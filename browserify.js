9b28629ece7ee0bf9dc40b4c3234b5d6864051ed
Richard Uggelberg <contact@richarduggelberg.com>
2020-04-19 13:14:07 +0200

    browserify

--- browserify.js
+++ browserify.js
@@ -141,6 +141,27 @@ function get_population(data) {
     return data[0].population;
 }
 
+/*
+ * Input: a date string in the format YYYY-MM-DD (not zero-padded)
+ * Output: the zero-padded date string, or "" if anything goes wrong.
+ */
+function pad_date(date_string) {
+    if (typeof(date_string) != "string") {
+        return "";
+    }
+    var parts = date_string.split("-");
+    if (parts.length != 3) {
+        return "";
+    }
+    if (parts[1].length == 1) {
+        parts[1] = "0"+parts[1];
+    }
+    if (parts[2].length == 1) {
+        parts[2] = "0"+parts[2];
+    }
+    return parts[0]+"-"+parts[1]+"-"+parts[2];
+}
+
 function remove_date_padding(padded_date) {
     var year = padded_date.slice(0,4);
     var month = padded_date.slice(5,7);
@@ -297,12 +318,13 @@ function make_prediction(sir_data, extra_days, past_days) {
     return prediction;
 }
 
+
+
 function addDays(date, days) {
     var result = new Date(date);
     result.setDate(result.getDate() + days);
     result = result.getFullYear() + "-" + 
-    (result.getMonth()+1) + "-" + (result.getDate());
-    console.log(result);
+    (result.getMonth()+1) + "-" + (result.getDate());    
     return result;
 }
 
@@ -347,7 +369,7 @@ module.exports = {
     url_to_covid_data, url_to_population_data, url_to_json,
     get_sir_from_index, get_population, get_index_of_date,
     make_chart, get_sirs_between_dates, get_start_and_end_date, 
-    make_prediction, remove_date_padding
+    make_prediction, remove_date_padding, pad_date
 
 }
 
